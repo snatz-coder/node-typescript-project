a2ed8e7b2b78ce253d03385cc4814d2d002d07f9
sujane <snazlopez@gmail.com>
2021-05-19 17:24:16 +0530

    created a function to concatenate array items

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("project setup");
+/**
+ * Split the `items` array into multiple, smaller arrays of the given `size`.
+
+ */
+ function chunk (items: any[], chunkSize:number) {  
+    const arraySet = []
+    items = [].concat(...items)
+
+  }
+  
+  
+ 
